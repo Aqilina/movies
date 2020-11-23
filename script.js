134e62d06a27f888466b635e69ca84f423f21d91
Tadas Matusevicius <akvilina.lapenaite@gmail.com>
2020-11-23 15:52:26 +0200

    2nd

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let movies = [
     {
+        id: "1",
         image: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
         title: "Parasite",
         year: "2019",
@@ -8,6 +9,7 @@ let movies = [
         comments: []
     },
     {
+        id: "2",
         image: "https://m.media-amazon.com/images/M/MV5BM2EwMmRhMmUtMzBmMS00ZDQ3LTg4OGEtNjlkODk3ZTMxMmJlXkEyXkFqcGdeQXVyMjM5ODk1NDU@._V1_UX182_CR0,0,182,268_AL_.jpg",
         title: "The Queen's Gambit ",
         year: "2020",
@@ -19,6 +21,7 @@ let movies = [
         }]
     },
     {
+        id: "3",
         image: "https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
         title: "Joker ",
         year: "2019",
@@ -36,6 +39,7 @@ let movies = [
         ]
     },
     {
+        id: "4",
         image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
         title: "The Godfather",
         year: "1972",
@@ -44,6 +48,7 @@ let movies = [
         comments: []
     },
     {
+        id: "5",
         image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg",
         title: "Pulp Fiction",
         year: "1994",
@@ -52,6 +57,7 @@ let movies = [
         comments: []
     },
     {
+        id: "6",
         image: "https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
         title: "The Shining",
         year: "1980",
@@ -66,6 +72,11 @@ let movies = [
 
 
 const containerFirst = document.querySelector('.containerFirst')
+const containerSecond = document.querySelector('.containerSecond')
+
+
+let chosenMovie
+
 
 function createCards(){
     containerFirst.innerHTML = ""
@@ -73,7 +84,7 @@ function createCards(){
     movies.map(item => {
         containerFirst.innerHTML += `
     
-        <div class="card">
+        <div class="card" id="${item.id}" onclick="chooseMovie(event)">
             <img src="${item.image}">
             <h4 class="titleName">${item.title}</h4>
             <div>${item.year}</div>
@@ -83,3 +94,28 @@ function createCards(){
     })
 }
 createCards()
+
+function chooseMovie(event){
+    // console.log(event)
+
+    let id
+    !!event.target.id ? id = event.target.id : id = event.path[1].id
+
+    chosenMovie = movies.filter(item => item.id === id)[0]
+
+    // console.log(chosenMovie)
+    // console.log(id)
+    create2ndPage()
+}
+
+
+function create2ndPage(){
+    containerSecond.innerHTML =''
+
+        containerSecond.innerHTML += `
+             <div class="movieInfoSide">
+                <img src="${chosenMovie.image}">
+                <div class="description">${chosenMovie.description}<div>
+            </div>
+        `
+}
